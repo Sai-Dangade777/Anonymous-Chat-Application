9402fdb857003efa93d2cc42f123b84d164839c8
saiprasad-frontend <sai3dangade@gmail.com>
2025-05-01 02:57:38 +0530

    Spam/Profanity Filter

--- app.js
+++ app.js
@@ -6,9 +6,27 @@ const server = app.listen(PORT, () => console.log(`💬 server on port ${PORT}`)
 
 const io = require('socket.io')(server)
 
+const Filter = require('bad-words');
+const customFilter = new Filter();
+customFilter.replaceWord = function() {
+  return '****';
+};
+console.log(customFilter.clean('You are a bitch')); // Should output: You are a ****
+console.log(customFilter.clean('You are a chutiya')); // Should output: You are a *******
+console.log(customFilter.clean('You are a bhosdi')); // Should output: You are a ******
+
+// Optionally add custom Hindi/other words
+customFilter.addWords(
+  'chutiya', 'bhosdi', 'bhosdike', 'madarchod', 'behenchod', 'benchod', 'gaand', 'loda', 'lund', 'randi', 'gandu', 'chodu', 'saala', 'harami', 'kutte', 'kamina', 'chutiye', 'bhenchod', 'mc', 'bc'
+);
+
 app.use(express.static(path.join(__dirname, 'public')))
 
 let socketsConected = new Set()
+
+function filterProfanity(message) {
+  return customFilter.clean(message);
+}
 
 io.on('connection', onConnected)
 
@@ -24,7 +42,8 @@ function onConnected(socket) {
   })
 
   socket.on('message', (data) => {
-    // console.log(data)
+    // Filter profanity before broadcasting
+    data.message = filterProfanity(data.message);
     socket.broadcast.emit('chat-message', data)
   })
 
