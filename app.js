5ec185a48d9060f4b99b0e12a7cc3c05215d52ef
saiprasad-frontend <sai3dangade@gmail.com>
2025-05-01 18:49:29 +0530

    fix: allow image file selection in file input and clean up moment.js script tag in index.html

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const express = require('express')
 const path = require('path')
 const app = express()
 
+app.use(express.json({ limit: '10mb' }));
+app.use(express.urlencoded({ limit: '10mb', extended: true }));
+
 const http = require('http');
 const server = http.createServer(app);
 
